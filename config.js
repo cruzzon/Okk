1604848e4514eee60f19f40a215f0b38416c81f6
cruzzon <gowthamchoww@gmail.com>
2025-01-03 16:01:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: "add your token",
+    prefix: "your prefix",
+    allowedUserIDs: ['1246094282985570388', '#', '#'], // Replace '#' with actual user IDs
+    allowedNoPrefixUserIDs: ['1246094282985570388'], // Users allowed to use commands without a prefix
+    ltcAddress: 'LcPnFkTa5UTav5Ue3dM6GdLh7LpTm47JZx',
+    paypalID: 'your-paypal-id@example.com'
+};
